378f7a2abea332ad4f6101078245ae7867f5473d
Tony Tamplin <tonytamps@gmail.com>
2017-10-09 14:08:14 +1100

    update the now playing message

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const config = require('./config')
 const server = require('./server')
 const client = require('./client')
 
-client.on('ready', () => server.init(client).listen(config.port))
+client.on('ready', () => {
+  server.init(client).listen(config.port)
+  client.user.setGame(`WoW | ${config.commandPrefix}help`); //Set the "Playing" text
+})
 
 client.login(process.env.DISCORD_API_KEY)
 
